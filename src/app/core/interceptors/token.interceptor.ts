b205e4aa0401815fa587cd81894bf5bee191d2be
Isaac Godínez <bezisaac@gmail.com>
2024-05-13 08:12:36 -0600

    se modifica item del LS userData por token

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,22 +1,16 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { DataResponse } from '../../auth/interfaces/login-response.interface';
-
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
     let clonedRequest= req;
-    let lsdata:DataResponse={} as DataResponse
-    if (localStorage.getItem("userdata")) {
-      lsdata=JSON.parse(localStorage.getItem("userdata")!)
-
+    if (localStorage.getItem("token")) {
       clonedRequest= req.clone({
-        setHeaders:{Authorization:lsdata.token!}
+        setHeaders:{Authorization:localStorage.getItem("token")!}
       })
     }
 
